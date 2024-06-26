ec962a322b7e8f77b90e6cac9f96141a5d61b30f
John Francis <johnmosima7@gmail.com>
2024-06-26 11:48:49 +0300

    updated env vars

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)ub-k9mevndz1%z!8*^o_y2c2@%n0pimrxr$410j116w1ifpc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['.vercel.app', 'movie.johnfrancis.dev', 'movielandingpage.onrender.com' '127.0.0.1' 'localhost']
+ALLOWED_HOSTS = ['os.environ.get("ALLOWED_HOSTS").split(" ")']
 
 
 # Application definition
