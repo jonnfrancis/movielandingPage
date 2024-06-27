fa807e30c673a0b3123fbd602eac70930cc4e9b4
John Francis <johnmosima7@gmail.com>
2024-06-27 12:08:03 +0300

    implemented cache

--- movie/views.py
+++ movie/views.py
@@ -1,37 +1,33 @@
 from django.shortcuts import render, redirect
-from django.core.cache import cache
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.conf import settings
 import random
 import secrets
-from django.urls import reverse
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 from .models import *
 
 import json
-import logging
-
-logger = logging.getLogger(__name__)
-
-CACHE_TTL = getattr(settings, 'CACHE_TTL', 60 * 15)
    
 
 # Create your views here.
 
-def index(request):
-    try:
-        cached_data = cache.get('index_view_data')
-        if cached_data:
-            logger.info("Index view data loaded from cache.")
-            return render(request, 'movie/index.html', cached_data)
-    except Exception as e:
-        logger.error(f"Error accessing cache: {e}")
+CACHE_TTL = 60 * 15
 
-    movies = list(Movie.objects.prefetch_related('get_pictures', 'get_background').all())
+# Cache the entire index view for 15 minutes
+@cache_page(CACHE_TTL)
+def index(request):
+    # Use prefetch_related to minimize database hits
+    movies = Movie.objects.prefetch_related('get_pictures', 'get_background').all()
+
+    # Precompute the pictures
+    for movie in movies:
+        movie.Picture = movie.get_pictures.filter(id=movie.id)
+
+    # Randomize cool_movies using Python's random module
     cool_movies = list(Movie.objects.filter(cool=True))
     random.shuffle(cool_movies)
 
-    random_movie = secrets.choice(movies)    
+    random_movie = secrets.choice(movies)
     random_movie2 = secrets.choice(movies)
     number = random.randint(1, 10)
     
@@ -44,19 +40,16 @@ def index(request):
         "random2": random_movie2,
         "number": number
     }
-
-    cache.set('index_view_data', context, CACHE_TTL)
-    logger.info("Index view data set to cache.")
     
     return render(request, 'movie/index.html', context)
 
-
+@cache_page(CACHE_TTL)
 def category(request):
-    category_id = request.GET.get('categories', None)
-    if category_id is None:
-        movies = Movie.objects.filter(kindaCool=True)
-    else:
+    category_id = request.GET.get('categories')
+    if category_id:
         movies = Movie.objects.filter(categories=category_id)
+    else:
+        movies = Movie.objects.filter(kindaCool=True)
     categories = Category.objects.all()
     
     return render(request, 'movie/categories.html', {
@@ -65,15 +58,13 @@ def category(request):
         "movies": movies
     })
 
-
+@cache_page(CACHE_TTL)
 def actor(request):
-    category_id = request.GET.get('category', None) 
-    
-    if category_id is None:
-        movies = Movie.objects.filter(kindaCool=True)
-    else:
+    category_id = request.GET.get('category')
+    if category_id:
         movies = Movie.objects.filter(actors__id=category_id)
-    
+    else:
+        movies = Movie.objects.filter(kindaCool=True)
     categories = Actor.objects.all()
     
     return render(request, 'movie/actors.html', {
@@ -82,48 +73,45 @@ def actor(request):
         'movies': movies,
     })
 
+@cache_page(CACHE_TTL)
 def director(request):
-    category_id = request.GET.get('category', None)
-    
-    if category_id is None:
-        movies = Movie.objects.filter(kindaCool=True)
-    else:
+    category_id = request.GET.get('category')
+    if category_id:
         movies = Movie.objects.filter(directors__id=category_id)
-    
+    else:
+        movies = Movie.objects.filter(kindaCool=True)
     categories = Director.objects.all()
     
     return render(request, 'movie/directors.html', {
         'categories': categories,
         'category_id': category_id,
         'movies': movies,
-    })  
+    })
 
+@cache_page(CACHE_TTL)
 def moviePage(request, movie_id):
-    try:
-        movie = cache.get(movie_id)
-        if not movie:
-            movie = Movie.objects.get(id=movie_id)
-            cache.set(movie_id, movie, timeout=CACHE_TTL)
-        logger.info(f"Movie {movie_id} loaded from cache.")
-    except Exception as e:
-        logger.error(f"Error accessing cache: {e}")
+    movie = cache.get(f'movie_{movie_id}')
+    if not movie:
         try:
             movie = Movie.objects.get(id=movie_id)
+            cache.set(f'movie_{movie_id}', movie, timeout=CACHE_TTL)
         except Movie.DoesNotExist:
             return redirect('/')
     
-    return render(request, 'movie/page.html', {
-        "movie": movie, 
+    context = {
+        "movie": movie,
         "backgroundMovie": movie.get_background.all(),
         "pictureMovie": movie.get_pictures.all()
-    })
+    }
+    return render(request, 'movie/page.html', context)
 
+@cache_page(CACHE_TTL)
 def type_view(request):
-    type_id = request.GET.get('type_id', None)
-    if type_id is None:
-        movies = Movie.objects.filter(kindaCool=True)
-    else:
+    type_id = request.GET.get('type_id')
+    if type_id:
         movies = Movie.objects.filter(type_id=type_id)
+    else:
+        movies = Movie.objects.filter(kindaCool=True)
     types = Type.objects.all()
 
     return render(request, 'movie/types.html', {
@@ -132,10 +120,8 @@ def type_view(request):
         "movies": movies
     })
 
-def workon(request): 
+def workon(request):
     message = "This is still under construction"
-    return render(request, 'movie/wait.html',{
+    return render(request, 'movie/wait.html', {
         "message": message
-    })   
-
-
+    })
