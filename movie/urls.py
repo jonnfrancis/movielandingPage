5fb5af980dbb85880600e68c7569aedd5f09c653
John Francis <johnmosima7@gmail.com>
2024-07-20 13:50:18 +0300

    fixed robots url

--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 import debug_toolbar
 from django.conf import settings
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
 
 from . import views
 from .views import MovieJsonListView
@@ -20,7 +20,7 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("movies-json/<int:num_movies>/", MovieJsonListView.as_view(), name='movies_json'),
     path("categories", views.category, name='category'),
-    path("robots.txt/", TemplateView.as_view(template_name="robots.txt", content_type="text/plain") ), 
+    path("robots.txt/", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")), 
     path('sitemap.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path("actors", views.actor, name='actor'),
     path("directors", views.director, name='director'),
